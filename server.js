97fa960e1ed0e83cb55f9ab34c463f74b822ea18
IsaacOlmstead <olmstead.isaac1234@gmail.com>
2026-03-04 16:21:40 -0500

    Updated server stuff

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(serveDir, "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
